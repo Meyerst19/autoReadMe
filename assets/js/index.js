8fbf6a4a95a5183ce625e54c6bef7bda61267656
TME <tyler.s.meyers@gmail.com>
2024-03-22 03:04:53 -0600

    up and running still need to make adjustments to the indentation formatting but after manual adjustment it is working

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,7 +27,7 @@ inquirer
     {
       type: "input",
       message: colors.brightMagenta("GitHub Username:"),
-      name: "gitHubUserName",
+      name: "gitHubUsername",
     },
     {
       type: "input",
@@ -46,13 +46,13 @@ inquirer
     },
     {
       type: "input",
-      message: colors.brightMagenta("Url:"),
-      name: "url",
+      message: colors.brightMagenta("Logo:"),
+      name: "logo",
     },
     {
       type: "input",
-      message: colors.brightMagenta("Logo:"),
-      name: "logo",
+      message: colors.brightMagenta("Screenshot:"),
+      name: "screenshot",
     },
     {
       type: "input",
@@ -82,7 +82,7 @@ inquirer
     {
       type: "checkbox",
       message: colors.brightMagenta("Built with:"),
-      name: "acknowlegments",
+      name: "built",
       choices: [
         "Next.js",
         "React.js",
@@ -96,7 +96,196 @@ inquirer
     },
   ])
   .then((response) => {
-    const formattedResponse = ``;
+    const npm = "```";
+    const npmSingle = "`";
+    const formattedResponse = `<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+
+    <a name="readme-top"></a>
+    
+    [![Contributors][contributors-shield]][contributors-url]
+    [![Forks][forks-shield]][forks-url]
+    [![Stargazers][stars-shield]][stars-url]
+    [![Issues][issues-shield]][issues-url]
+    [![MIT License][license-shield]][license-url]
+    [![LinkedIn][linkedin-shield]][linkedin-url]
+    
+    <br />
+    <div align="center">
+      <a href="https://github.com/${response.gitHubUsername}/${response.repoName}">
+        <img src="./assets/images/${response.logo}" alt="Logo" width="80" height="80">
+      </a>
+    
+    <h3 align="center">${response.projectTitle}</h3>
+    
+      <p align="center">
+        ${response.projectDescription}
+        <br />
+        <a href="https://github.com/${response.gitHubUsername}/${response.repoName}"><strong>Explore the docs »</strong></a>
+        <br />
+        <br />
+        <a href="https://github.com/${response.gitHubUsername}/${response.repoName}">View Demo</a>
+        ·
+        <a href="https://github.com/${response.gitHubUsername}/${response.repoName}/issues">Report Bug</a>
+        ·
+        <a href="https://github.com/${response.gitHubUsername}/${response.repoName}/issues">Request Feature</a>
+      </p>
+    </div>
+    
+    <details>
+      <summary>Table of Contents</summary>
+      <ol>
+        <li>
+          <a href="#about-the-project">About The Project</a>
+          <ul>
+            <li><a href="#built-with">Built With</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#getting-started">Getting Started</a>
+          <ul>
+            <li><a href="#prerequisites">Prerequisites</a></li>
+            <li><a href="#installation">Installation</a></li>
+          </ul>
+        </li>
+        <li><a href="#usage">Usage</a></li>
+        <li><a href="#roadmap">Roadmap</a></li>
+        <li><a href="#contributing">Contributing</a></li>
+        <li><a href="#license">License</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#acknowledgments">Acknowledgments</a></li>
+      </ol>
+    </details>
+    
+    ## About The Project
+    
+    [![Product Name Screen Shot][product-screenshot]](https://${response.url})
+     
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ### Built With
+    
+    - [![Next][${response.built}]][Next-url]
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## Getting Started
+    
+    To get a local copy up and running follow these simple example steps.
+    
+    ### Prerequisites
+    
+    ${response.dependencies}
+    
+    - npm
+      ${npm}sh
+      npm install npm@latest -g
+      ${npm}
+    
+    ### Installation
+    
+    1. Get a free API Key at [https://${response.url}](https://${response.url})
+    2. Clone the repo
+       ${npm}sh
+       git clone https://github.com/${response.gitHubUsername}/${response.repoName}.git
+       ${npm}
+    3. Install NPM packages
+       ${npm}sh
+       npm install
+       ${npm}
+    4. Enter your API in ${npmSingle}config.js${npmSingle}
+       ${npm}js
+       const API_KEY = "ENTER YOUR API";
+       ${npm}
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## Usage
+    
+    ${response.useage}
+    
+    _For more examples, please refer to the [Documentation](${response.url})_
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## Roadmap
+    
+    - [ ] ${response.roadmap}
+      - [ ] Nested Feature
+    
+    See the [open issues](https://github.com/${response.gitHubUsername}/${response.repoName}/issues) for a full list of proposed features (and known issues).
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## Contributing
+    
+    Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+    
+    If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+    Don't forget to give the project a star! Thanks again!
+    
+    1. Fork the Project
+    2. Create your Feature Branch (${npmSingle}git checkout -b feature/AmazingFeature${npmSingle})
+    3. Commit your Changes (${npmSingle}git commit -m 'Add some AmazingFeature'${npmSingle})
+    4. Push to the Branch (${npmSingle}git push origin feature/AmazingFeature${npmSingle})
+    5. Open a Pull Request
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## License
+    
+    Distributed under the MIT License. See ${npmSingle}LICENSE.txt${npmSingle} for more information.
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## Contact
+    
+    ${response.name} - [@${response.twitterHandle}](https://twitter.com/${response.twitterHandle}) - ${response.email}
+    
+    Project Link: [https://github.com/${response.gitHubUsername}/${response.repoName}](https://github.com/${response.gitHubUsername}/${response.repoName})
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    ## Acknowledgments
+    
+    - [${response.acknowlegments}]()
+    - []()
+    - []()
+    
+    <p align="right">(<a href="#readme-top">back to top</a>)</p>
+    
+    <!-- MARKDOWN LINKS & IMAGES -->
+    <!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+    
+    [contributors-shield]: https://img.shields.io/github/contributors/${response.gitHubUsername}/${response.repoName}.svg?style=for-the-badge
+    [contributors-url]: https://github.com/${response.gitHubUsername}/${response.repoName}/graphs/contributors
+    [forks-shield]: https://img.shields.io/github/forks/${response.gitHubUsername}/${response.repoName}.svg?style=for-the-badge
+    [forks-url]: https://github.com/${response.gitHubUsername}/${response.repoName}/network/members
+    [stars-shield]: https://img.shields.io/github/stars/${response.gitHubUsername}/${response.repoName}.svg?style=for-the-badge
+    [stars-url]: https://github.com/${response.gitHubUsername}/${response.repoName}/stargazers
+    [issues-shield]: https://img.shields.io/github/issues/${response.gitHubUsername}/${response.repoName}.svg?style=for-the-badge
+    [issues-url]: https://github.com/${response.gitHubUsername}/${response.repoName}/issues
+    [license-shield]: https://img.shields.io/github/license/${response.gitHubUsername}/${response.repoName}.svg?style=for-the-badge
+    [license-url]: https://github.com/${response.gitHubUsername}/${response.repoName}/blob/master/LICENSE.txt
+    [linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+    [linkedin-url]: https://linkedin.com/in/${response.linkedInUsername}
+    [product-screenshot]: ./assets/images/${response.screenshot}
+    [Next.js]: https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white
+    [Next-url]: https://nextjs.org/
+    [React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+    [React-url]: https://reactjs.org/
+    [Vue.js]: https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D
+    [Vue-url]: https://vuejs.org/
+    [Angular.io]: https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white
+    [Angular-url]: https://angular.io/
+    [Svelte.dev]: https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00
+    [Svelte-url]: https://svelte.dev/
+    [Laravel.com]: https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white
+    [Laravel-url]: https://laravel.com
+    [Bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
+    [Bootstrap-url]: https://getbootstrap.com
+    [JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
+    [JQuery-url]: https://jquery.com
+    `;
     fs.appendFile("README.md", formattedResponse, (err) =>
       err
         ? console.error(colors.red(err))
